ee2171e7cf9be4c86895ac5d98bb0762661e8ff7
Anthony <100pour100inde@gmail.com>
2021-08-28 16:58:29 +0200

    Almost done

--- js/index.js
+++ js/index.js
@@ -6,22 +6,42 @@ let collapse1 = document.getElementById('apropos-list-1')
 let collapse2 = document.getElementById('apropos-list-2')
 
 div1.addEventListener('click', () => {
-    if (collapse1.classList.contains('show')) {
-        triangle1.style.transform = 'rotate(0deg)'
-        triangle1.style.borderLeft = '43px solid #dcb36a'
+    if (window.matchMedia('(min-width: 992px)').matches) {
+        if (collapse1.classList.contains('show')) {
+            triangle1.style.transform = 'rotate(0deg)'
+            triangle1.style.borderLeft = '43px solid #dcb36a'
+        } else {
+            triangle1.style.transform = 'rotate(90deg)'
+            triangle1.style.borderLeft = '43px solid #D13978'
+        }
     } else {
-        triangle1.style.transform = 'rotate(90deg)'
-        triangle1.style.borderLeft = '43px solid #D13978'
+        if (collapse1.classList.contains('show')) {
+            triangle1.style.transform = 'rotate(0deg)'
+            triangle1.style.borderLeft = '30px solid #dcb36a'
+        } else {
+            triangle1.style.transform = 'rotate(90deg)'
+            triangle1.style.borderLeft = '30px solid #D13978'
+        }
     }
 })
 
 div2.addEventListener('click', () => {
-    if (collapse2.classList.contains('show')) {
-        triangle2.style.transform = 'rotate(0deg)'
-        triangle2.style.borderLeft = '43px solid #dcb36a'
+    if (window.matchMedia('(min-width: 992px)').matches) {
+        if (collapse2.classList.contains('show')) {
+            triangle2.style.transform = 'rotate(0deg)'
+            triangle2.style.borderLeft = '43px solid #dcb36a'
+        } else {
+            triangle2.style.transform = 'rotate(90deg)'
+            triangle2.style.borderLeft = '43px solid #D13978'
+        }
     } else {
-        triangle2.style.transform = 'rotate(90deg)'
-        triangle2.style.borderLeft = '43px solid #D13978'
+        if (collapse2.classList.contains('show')) {
+            triangle2.style.transform = 'rotate(0deg)'
+            triangle2.style.borderLeft = '30px solid #dcb36a'
+        } else {
+            triangle2.style.transform = 'rotate(90deg)'
+            triangle2.style.borderLeft = '30px solid #D13978'
+        }
     }
 })
 
